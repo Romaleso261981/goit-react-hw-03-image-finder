4f03af82aa7c69659556f5960af3ff7c2473ba54
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-10-24 15:21:18 +0300

    create component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,82 +1,65 @@
-import { FilterInput, NotificationSpan } from './AppStyle.js';
-import { Component } from 'react';
-import Notification from './Notification/Notification';
-import { nanoid } from 'nanoid';
-import ContactList from './ContactList/ContactList';
-import ContactForm from './ContactForm/ContactForm';
+import { Component } from 'react';;
 
 export default class App extends Component {
   state = { contacts: [], filter: '' };
 
-  componentDidMount() {
-    const contactLocal = JSON.parse(localStorage.getItem('contacts'));
-    if (contactLocal === null) return;
-    this.setState({ contacts: contactLocal });
-  }
+  // componentDidMount() {
+  //   const contactLocal = JSON.parse(localStorage.getItem('contacts'));
+  //   if (contactLocal === null) return;
+  //   this.setState({ contacts: contactLocal });
+  // }
 
   
-  componentDidUpdate() {
-    const {contacts} = this.state
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }
+  // componentDidUpdate() {
+  //   const {contacts} = this.state
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }
 
-  handleChange = e => {
-    this.setState({ name: e.target.value });
-  };
+  // handleChange = e => {
+  //   this.setState({ name: e.target.value });
+  // };
 
-  addContact = contact => {
-    const { contacts } = this.state;
-    if (
-      contacts.filter(({ number }) => number === contact.number).length !== 0
-    ) {
-      alert(contact.number + ' this number is already in your phone book');
-      return;
-    }
-    this.setState(prevState => {
-      const newContact = { id: nanoid(), ...contact };
-      return {
-        contacts: [...prevState.contacts, newContact],
-      };
-    });
+  // addContact = contact => {
+  //   const { contacts } = this.state;
+  //   if (
+  //     contacts.filter(({ number }) => number === contact.number).length !== 0
+  //   ) {
+  //     alert(contact.number + ' this number is already in your phone book');
+  //     return;
+  //   }
+  //   this.setState(prevState => {
+  //     const newContact = { id: nanoid(), ...contact };
+  //     return {
+  //       contacts: [...prevState.contacts, newContact],
+  //     };
+  //   });
     
-  };
+  // };
 
-  deleteContact = id => {
-    this.setState(({ contacts }) => {
-      const updatedContacts = contacts.filter(contact => contact.id !== id);
-      localStorage.setItem('contacts', JSON.stringify(updatedContacts));
-      return { ...this.state, contacts: updatedContacts };
-    });
-  };
+  // deleteContact = id => {
+  //   this.setState(({ contacts }) => {
+  //     const updatedContacts = contacts.filter(contact => contact.id !== id);
+  //     localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+  //     return { ...this.state, contacts: updatedContacts };
+  //   });
+  // };
 
-  handleFilter = e => {
-    this.setState({ filter: e.target.value });
-  };
+  // handleFilter = e => {
+  //   this.setState({ filter: e.target.value });
+  // };
 
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state;
+  // getFilteredContacts = () => {
+  //   const { contacts, filter } = this.state;
 
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  };
+  //   return contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(filter.toLowerCase())
+  //   );
+  // };
 
   render() {
-    const filteredContacts = this.getFilteredContacts();
     return (
       <>
-        <ContactForm addContact={this.addContact} value={20} />
-        <FilterInput type="text" onChange={this.handleFilter} />
-        {filteredContacts.length > 0 ? (
-          <ContactList
-            contactsData={filteredContacts}
-            deleteContact={this.deleteContact}
-          />
-        ) : (
-          <NotificationSpan>
-            <Notification message="No contacts yet" />
-          </NotificationSpan>
-        )}
+        helo world
       </>
     );
   }
